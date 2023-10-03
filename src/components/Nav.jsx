d5353c75606284656efea4d4d41cf53c752c9bf4
Scharly OPITZ <scharly.opitz@gmail.com>
2023-10-03 13:13:40 +0200

    modification animation nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -40,89 +40,62 @@ export default function Nav({ base, burgerActive, setBurgerActive }) {
                 initial={{ y: "calc(-100% + -100px)" }}
                 animate={{
                     y: 0,
-                    transition: { duration: 1, ease: [0.65, 0, 0.35, 1] },
+                    transition: { duration: 0.8, ease: [0.65, 0, 0.35, 1] },
                 }}
                 exit={{
                     y: "calc(-100% + -100px)",
                     transition: {
-                        duration: 0.5,
+                        duration: 0.8,
                         ease: [0.65, 0, 0.35, 1],
                         delay: 0.05,
                     },
                 }}
             >
-                <motion.div
-                    className="nav"
-                    initial={{ height: "100%" }}
-                    animate={{ height: "70%", transition: { duration: 1 } }}
-                >
-                    <div className="text-container">
-                        {Navs.map((nav, idx) => (
-                            <NavLink
-                                onClick={() => setBurgerActive(false)}
-                                to={`${base}${nav.to}`}
-                                onMouseEnter={(e) => handleMouseEnter(e, idx)}
-                                key={idx}
+                <div className="text-container">
+                    {Navs.map((nav, idx) => (
+                        <NavLink
+                            onClick={() => setBurgerActive(false)}
+                            to={`${base}${nav.to}`}
+                            onMouseEnter={(e) => handleMouseEnter(e, idx)}
+                            key={idx}
+                        >
+                            <motion.div
+                                initial={{ y: "100%" }}
+                                animate={{
+                                    y: 0,
+                                    transition: {
+                                        duration: 0.5,
+                                        delay: 0.6,
+                                        ease: [0.65, 0, 0.35, 1],
+                                    },
+                                }}
+                                exit={{
+                                    y: "100%",
+                                    transition: {
+                                        ease: [0.65, 0, 0.35, 1],
+                                    },
+                                }}
                             >
-                                <motion.div
-                                    initial={{ y: "100%" }}
+                                <motion.p
+                                    data-id={idx}
                                     animate={{
-                                        y: 0,
-                                        transition: {
-                                            duration: 0.5,
-                                            delay: 0.6,
-                                            ease: [0.65, 0, 0.35, 1],
-                                        },
-                                    }}
-                                    exit={{
-                                        y: "100%",
-                                        transition: {
-                                            ease: [0.65, 0, 0.35, 1],
-                                        },
+                                        y: data === idx ? "-100%" : 0,
                                     }}
                                 >
-                                    <motion.p
-                                        data-id={idx}
-                                        animate={{
-                                            y: data === idx ? "-100%" : 0,
-                                        }}
-                                    >
-                                        {nav.name}
-                                    </motion.p>
-                                    <motion.p
-                                        data-id={idx}
-                                        animate={{
-                                            y: data === idx ? "-100%" : 0,
-                                        }}
-                                    >
-                                        {nav.name}
-                                    </motion.p>
-                                </motion.div>
-                            </NavLink>
-                        ))}
-                    </div>
-                    <svg>
-                        <motion.path
-                            initial={{ d: initialPath }}
-                            animate={{
-                                d: animatedPath,
-                                transition: {
-                                    duration: 1,
-                                    ease: [0.65, 0, 0.35, 1],
-                                },
-                            }}
-                            exit={{
-                                d: initialPath,
-                                transition: {
-                                    duration: 0.5,
-                                    ease: [0.65, 0, 0.35, 1],
-                                    delay: 0.1,
-                                },
-                            }}
-                            fill="#f5deb3"
-                        />
-                    </svg>
-                </motion.div>
+                                    {nav.name}
+                                </motion.p>
+                                <motion.p
+                                    data-id={idx}
+                                    animate={{
+                                        y: data === idx ? "-100%" : 0,
+                                    }}
+                                >
+                                    {nav.name}
+                                </motion.p>
+                            </motion.div>
+                        </NavLink>
+                    ))}
+                </div>
 
                 <svg>
                     <motion.path
@@ -130,7 +103,7 @@ export default function Nav({ base, burgerActive, setBurgerActive }) {
                         animate={{
                             d: animatedPath,
                             transition: {
-                                duration: 1,
+                                duration: 1.2,
                                 ease: [0.65, 0, 0.35, 1],
                             },
                         }}
